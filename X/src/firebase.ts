c6ecb2b3d8d5b8767fbe99295f264573e5c66bfc
[AI9] 김수산 <psm77991133@gmail.com>
2023-12-11 21:06:01 +0900

    feat: 12/11

---
+++ X/src/firebase.ts
@@ -0,0 +1,12 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyAptAzvHuajWUz97MLt4-XjKoeJs5z2JM4",
+  authDomain: "clonex-161ee.firebaseapp.com",
+  projectId: "clonex-161ee",
+  storageBucket: "clonex-161ee.appspot.com",
+  messagingSenderId: "410990139550",
+  appId: "1:410990139550:web:b4857ab43c0fd2571a438f",
+};
+
+const app = initializeApp(firebaseConfig);
